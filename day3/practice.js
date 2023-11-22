4e7ebcdaf09b0795e0964c4213b81e58091319e1
mahmadasghar <muhammadahmadb1234@gmail.com>
2023-11-22 19:40:48 +0500

    upload changes in javascript

---
+++ day3/practice.js
@@ -0,0 +1,31 @@
+// Opreators:
+    //  Operation are specical symbols that perform operations on the values
+    // there are two cetagories ofoperators
+    // i-Uniry Operators ii- Bnay Operators
+
+    // 1- Uniry OPerators:
+        // These are the opreators that perform operation on only one value for example (++, --)
+
+let aNumber = 5
+let anotherNumber = 5
+
+anotherNumber = anotherNumber + aNumber++
+
+// Question: What will be the value of 'anotherNumber' after above operation?
+// console.log('aNumber', aNumber)
+// console.log('anotherNumber', anotherNumber)
+
+    // 2- Binary Operators:
+        // These are the opreators that perform operation on at least two values for example (+, -, * ,/)
+        // Binary Opreators are three types
+        // i- Arithmatic Opreators ii- Logical Opreators iii- Comparison Opreators
+            // 1- Arithmatic Opreators
+            // These are;
+                // + (Plus)
+                // - (minus)
+                // * (multiply)
+                // / (divide)
+                // % (modulas) (gives back the reminder)
+                    console.log('Modulus Result:', 123 % 9)
+                // ** (exponent) (calculates the power of the number)
+                    console.log('power 2 of 10: ', 10**2)
