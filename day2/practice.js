1fd2423b3618edb6dbcec35bf4e208bdbb9cc460
mahmadasghar <muhammadahmadb1234@gmail.com>
2023-11-22 18:18:54 +0500

    upload changes in javascript

--- day2/practice.js
+++ day2/practice.js
@@ -8,4 +8,42 @@
 //      i: name cannot start with a number
 //          let 4des = 134
 //      ii: name cannot contain any specical characters except for an underscore (_) (note: no space allowed as well)
-                        //  let varible
+                  //  let varible_dsfetfg = 45
+                  //  let getgdy3quq689whed£$%^& = 45
+//                iii: cannot use language reserved keywords (i.e. let, const, var, calss funcation, for , while, do, new, true, false etc)
+            // let let = 234
+            // let const = 32
+            // let class = 345 
+            // let funcation = 34
+            // let new = 98
+
+// 3: conventions for naming varibles
+//      i: Camel Case:
+            let someVariableIsThis = 123
+//      ii: Pascal Case:
+            let SomeVariableIsThis = 123
+//      iii: Snake Case:
+            let some_variable_is_this = 123
+//      iv: Capital Snake case: (used to name constant with hard coded values)
+                    let SOME_VARIABLE_IS_THIS = 123
+
+// 4: Data Types:
+        //  There are two cetagories of data types in javasacript.
+        // i- primitive Data types:
+                // i- integers/numbers
+                // ii- strings 
+                // iii-boolean
+
+
+                // 1- Integers/Numbers
+                        // i-integers: 1, 2, 3, 4, 78 etc
+                        // ii-float: 0.0004, 1.05,4.224 etc
+
+                // 2- Strings
+                        // sequence of mixed characters, alphabets, numbers, and specical charaters enclosed in '' or "" is known as string
+                        let someString = 'yghydjufghudjs"£$%^&*vfchndxjzmujxkz,345678'
+                        let anotherString = "dfgxhjdkc!%^&asdfghjklqqwertyuio1234567890'asdfghjkl;xcvbnm,./£$%^&&&&&*yuio"
+                // 3- Boolean
+                        // ture or false as values of the varible
+                        let ok = true
+                        let notOk = false
