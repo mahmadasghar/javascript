7d61b6a29b60f60744813a2fd6565f03f04f76ea
mahmadasghar <muhammadahmadb1234@gmail.com>
2024-01-06 13:15:42 +0500

    upload changes in javascript

--- day10/index.js
+++ day10/index.js
@@ -15,11 +15,29 @@
     //      code to be exceuted
     //  }
     //   while(condition)
-    let n = prompt("Enter the value of n")
-  n = Number.parseInt(n)
+  //   let n = prompt("Enter the value of n")
+  // n = Number.parseInt(n)
   
-  let i = 0;
-  do{
-    console.log(i)
-    i++;
-  }while(i<n)
+  // let i = 0;
+  // do{
+  //   console.log(i)
+  //   i++;
+  // }while(i<n)
+  /*----------------------FUNCATIONS IN JAVASCRIPT------------------------*/
+  //       A javascript funcation is a block of code designed to perform a  particular task.
+//  the syntax of a funcation looks something like the :
+//     funacation my fnuc(){// code to be exceuted}
+//              FOR EXAMPLE
+function oneplusAvg(x,y){
+  return 1+ (x+y)/2
+}
+// let a = 1
+// let b = 2
+// let c = 3
+// console.log("Average of a and b is" , (a+b)/2)
+// console.log("Average of c and b is" , (c+b)/2)
+// console.log("Average of a and c is" , (a+c)/2)
+const sum = (p,q)=>{
+  return p + q
+}
+
