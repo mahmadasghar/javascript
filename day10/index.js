750230571f32d84a2b952205031e363575bb4b00
mahmadasghar <muhammadahmadb1234@gmail.com>
2024-01-07 14:21:07 +0500

    upload changes in javascript

--- day10/index.js
+++ day10/index.js
@@ -34,10 +34,10 @@ function oneplusAvg(x,y){
 // let a = 1
 // let b = 2
 // let c = 3
-// console.log("Average of a and b is" , (a+b)/2)
-// console.log("Average of c and b is" , (c+b)/2)
-// console.log("Average of a and c is" , (a+c)/2)
+console.log("Average of a and b is" ,oneplusAvg(a+b)/2)
+console.log("Average of c and b is" , oneplusAvg(c+b)/2)
+console.log("Average of a and c is" , oneplusAvg(a+c)/2)
 const sum = (p,q)=>{
   return p + q
 }
-
+console
